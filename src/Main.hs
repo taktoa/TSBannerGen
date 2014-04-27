7d7f27c6de126b1fbf37341c723cf634978a10eb
Remy Goldschmidt <remy.goldschmidt@gmail.com>
2014-04-27 17:09:50 -0400

    Folded bash script into program, now self-contained CGI script. Requires ImageMagick, however.

--- src/Main.hs
+++ src/Main.hs
@@ -1,42 +1,29 @@
 module Main (main) where
 import Network.CGI
+import System.Process.ByteString (readProcessWithExitCode)
+import Data.ByteString.Lazy (fromStrict, toStrict)
+import Data.ByteString.Lazy.Builder (toLazyByteString, stringUtf8)
 import MusicStatus
-import Data.String.Utils (replace, split)
-import Data.Either
-import MPD as MPD
-import MOCP as MOCP
-import Utility (date, (//))
+import qualified MPD
+import qualified MOCP
+import GenSVG
+import Utility ((<$>))
 
 tmp = "template.tmp"
-font = "serif"
-style = "normal"
-image = "base.png"
-tsfmt = "%H:%M:%S %Z | %B %e, %Y"
+fnt = "serif"
+stl = "normal"
+img = "base.png"
+tft = "%H:%M:%S %Z | %B %e, %Y"
 
 main :: IO ()
 main = do
     ms <- MPD.getMusicStatus
-    let (MusicStatus fl ti ar al st pe) = ms
-    let file = if fl == "" then "" else take 54 $ last $ split "/" fl
-    let (title, artist, album) = (take 54 ti, take 54 ar, take 54 al)
-    let status = case st of
-            PLAY ->  "&#9654;"
-            PAUSE -> "&#10073;&#10073;"
-            STOP ->  "&#9726;"
-    let percent = show $ round $ (*100) $ uncurry (//) $ pe
-    timestamp <- date tsfmt
-    let runReplace = map (uncurry replace)
-                      [ ("SONG_FILE", file),
-                        ("SONG_TITLE", title),
-                        ("SONG_ARTIST", artist),
-                        ("SONG_ALBUM", album),
-                        ("SONG_STATUS", status),
-                        ("SONG_PERCENT", percent),
-                        ("INFO_FONT", font),
-                        ("INFO_STYLE", style),
-                        ("BANNER_IMAGE", image),
-                        ("TIMESTAMP", timestamp) ]
-    let replacer = (flip $ foldr ($)) runReplace
-    template <- readFile tmp
-    putStrLn $ replacer template
-    --runCGI $ handleErrors (output $ show st)
+    let ss = SVGSettings tmp fnt stl img tft
+    r <- genSVG ss ms
+    let svg = toStrict $ toLazyByteString $ stringUtf8 r
+    png <- readProcess "convert" ["svg:-", "-colors", "16", "png:-"] svg
+    runCGI $ handleErrors $ do
+        setHeader "Content-type" "image/png"
+        outputFPS $ fromStrict png
+    where
+    readProcess f a s = (\(_,x,_) -> x) <$> readProcessWithExitCode f a s
